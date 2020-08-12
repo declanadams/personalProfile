a4735e3e9eb7abdc03a3bec6b68b6352590f2625
Declan Adams <69381864+declanadams@users.noreply.github.com>
2020-08-12 20:40:35 +0200

    Added personalProfile files

---
+++ js/main.js
@@ -0,0 +1,17 @@
+function myFunction(moreid) {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById(moreid);
+    var btnText = document.getElementById("myBtn");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "block";
+    }
+
+
+}
